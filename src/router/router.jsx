86bd3aef436dba43c5dc158697dc1090d18f738a
kobirul5 <kobirul05j@gmail.com>
2024-12-11 00:05:25 +0600

    added stucture

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../layout/MainLayout';
+import Home from '../pages/Home/Home';
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element: "hello"
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
     }
 ])
 
