5deb9d8492c36ced05028f80c513f993fd3a56d8
kobirul5 <kobirul05j@gmail.com>
2024-12-11 00:21:29 +0600

    create some rout and add lottie json file

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
